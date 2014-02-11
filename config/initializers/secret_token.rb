94d16cfa4e0044fb5c46d944f3c926f617c9835c
sirramongabriel <sirramongabriel@gmail.com>
2014-02-11 12:17:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialLoginSandbox::Application.config.secret_token = 'a9639d5d85de73a1f3454a86afa19a833e8fc8182c8da5d7d67d03c0c19bc35d89cab1dc7d31159194c71f15ffd5f03d8061c6a4da90655b066ef84b70b06596'
